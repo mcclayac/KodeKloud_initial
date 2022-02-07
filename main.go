6a4411dce880407f05a4c2aade21bb3fb8df3eea
Anthony McClay <anthony.mcclay@gmail.com>
2022-02-07 07:33:35 -0600

    Second commit - variable functions

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 
@@ -9,6 +12,141 @@ func main() {
 	dataTypes()
 	printVariables()
 	printFormatted()
+	declaringVariables()
+	scopeVariable()
+	initialVariableValues()
+	scanfFunction()
+	findTheVariableType()
+	convertingDataTypes()
+
+}
+
+func convertingDataTypes() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("Converting Data Types\n")
+
+	fmt.Println("Interger to float")
+	var i = 90
+	var f float64 = float64(i)
+	fmt.Printf("%.2f\n", f)
+
+	fmt.Println("Float  to Integer")
+	var f2 float64 = 54.89
+	var i2 int = int(f2)
+	fmt.Printf("%v\n", i2)
+
+}
+
+func findTheVariableType() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("Finf variable Type\n")
+	var (
+		grades  int     = 42
+		message string  = "hello world"
+		isCheck bool    = true
+		amount  float32 = 5466.54
+	)
+	fmt.Printf("variable grades = %v is of type %T\n", grades, grades)
+	fmt.Printf("variable message = %v is of type %T\n", message, message)
+	fmt.Printf("variable isCheck = %v is of type %T\n", isCheck, isCheck)
+	fmt.Printf("variable amount = %v is of type %T\n\n", amount, amount)
+
+	fmt.Printf("Type: %v \n", reflect.TypeOf(1000))
+	fmt.Printf("Type: %v \n", reflect.TypeOf("priyanka"))
+	fmt.Printf("Type: %v \n", reflect.TypeOf(46.0))
+	fmt.Printf("Type: %v \n\n", reflect.TypeOf(true))
+
+	fmt.Printf("variable grades = %v is of type %v\n", grades, reflect.TypeOf(grades))
+	fmt.Printf("variable message = %v is of type %v\n", message, reflect.TypeOf(message))
+	fmt.Printf("variable isCheck = %v is of type %v\n", isCheck, reflect.TypeOf(isCheck))
+	fmt.Printf("variable amount = %v is of type %v\n\n", amount, reflect.TypeOf(amount))
+
+}
+
+func scanfFunction() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("scanf function\n")
+
+	//var name string
+	//fmt.Print("Enter your name: ")
+	//fmt.Scanf("%s", &name)
+	//fmt.Printf("\nHello %s\n", name)
+	//
+	//var (
+	//	name2      string
+	//	is_muggle2 bool
+	//)
+	//fmt.Printf("\nEnter your name & are you a muggle : ")
+	//fmt.Scanf("%s %t", &name2, &is_muggle2)
+	//fmt.Printf("Hello %s ... are you a muggle: %t\n", name2, is_muggle2)
+
+	//var (
+	//	a string
+	//	b int
+	//)
+	//fmt.Print("\nEnter a sting and a number :")
+	//count, err := fmt.Scanf("%s %d", &a, &b)
+	//fmt.Println("count : ", count)
+	//fmt.Println("err : ", err)
+	//fmt.Printf("a: %s b: %d", a, b)
+
+}
+
+func initialVariableValues() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("Initial Variable Values\n")
+
+	var i int
+	fmt.Printf("Inital vaiable value : %d", i)
+
+}
+
+func scopeVariable() {
+	fmt.Println("\n\n--------------------------------------------")
+	fmt.Println("Vaiable scope\n")
+	// blocks
+
+	city := "Markham"
+	{ // outter Block
+		country := "USA"
+
+		{ // inner block
+			contenent := "North America"
+			fmt.Printf("\ncity: %s, country: %s, contenent: %s\n", city, country, contenent)
+		}
+	}
+	// fmt.Printf("\ncity: %s, country: %s, contenent: %s\n", city, country, contenent)
+}
+
+func declaringVariables() {
+
+	fmt.Println("\n\nDeclaring variables")
+	fmt.Println("------------------------------")
+
+	var name string
+	name = "Tony"
+
+	fmt.Printf("\n name %s\n", name)
+
+	// shorthand #1
+	var s, t string = "foo", "bar"
+	fmt.Printf("\nPrinting foo,bar = %s, %s\n", s, t)
+
+	// shorthand #2
+	var (
+		s2 string = "Tony"
+		i2 int    = 52
+	)
+	fmt.Printf("\nName: %s Age: %d", s2, i2)
+
+	// short variable declaration
+	name3 := "Maria"
+	age3 := 40
+	fmt.Printf("\nWoman: %s age: %d", name3, age3)
+
+	name3 = "lisa"
+	age3 = 34
+	fmt.Printf("\nWoman: %s age: %d", name3, age3)
 
 }
 
@@ -16,8 +154,11 @@ func printFormatted() {
 
 	var city string = "Kolkata"
 	var user string = "macDady"
+	var grade int = 32
 
 	fmt.Printf("\n\ncity: %v usrname: %v \n", city, user)
+
+	fmt.Printf("\n-------------------\nprintf()\n\ncity: %v username: %v  grade %d", city, user, grade)
 }
 
 func printVariables() {
