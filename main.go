eb7390b28af68bce19d047b6a87d6cab6456caea
Anthony McClay <anthony.mcclay@gmail.com>
2022-02-11 18:26:50 -0600

    Maps continmued

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"reflect"
-	"rsc.io/quote"
 	"strconv"
 )
 
@@ -11,13 +10,275 @@ const PI = 3.14
 
 func main() {
 
-	fmt.Println(quote.Go())
+	// fmt.Println(quote.Go())
 
 	// Chapter 1
 	// dataTypesAndVariables()
 
 	//Chapter2
-	operatorsAndControlFlow()
+	// operatorsAndControlFlow()
+
+	// Chapter 3
+	arraysAndSlices()
+
+}
+
+func arraysAndSlices() {
+	fmt.Println("--------------------------------")
+	fmt.Println("Chapter 3")
+	fmt.Println("\n\n Arrays and Slices")
+
+	arrays()
+	slices()
+	maps()
+
+}
+
+func maps() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("maps {\n\n")
+
+	//fmt.Println("This is an error")
+	//var code map[string]string
+	//code["en"] = "English"
+	//fmt.Println(code)
+
+	codes2 := map[string]string{"en": "English", "fr": "French"}
+	fmt.Println("codes2 := map[string]string{\"en\":\"English\",\"fr\":\"French\"}")
+	fmt.Println("codes2 = ", codes2, "\n")
+
+	fmt.Println("Map with make function")
+	codes3 := make(map[string]int)
+	fmt.Println("codes3 := make(map[string]int) =", codes3)
+	fmt.Println("empty map")
+
+	codes4 := map[string]string{"en": "English", "fr": "French", "hi": "Hindi"}
+	fmt.Println("codes4 := map[string]string{\"en\": \"English\", \"fr\": \"French\",\"hi\":\"Hindi\"}")
+	fmt.Println("codes4 =", codes4)
+	fmt.Println("len(codes4) = ", len(codes4))
+
+	fmt.Println("Accessing the keys of an map - codes4")
+	fmt.Println("codes4[\"en\"] =", codes4["en"])
+	fmt.Println("codes4[\"fr\"] =", codes4["fr"])
+	fmt.Println("codes4[\"hi\"] =", codes4["hi"])
+
+	fmt.Println("\n ----- getting a value from a map - value, found")
+	code5 := map[string]int{"en": 1, "fr": 2, "hi": 3}
+	fmt.Println("code5 := map[string]int{\"en\": 1, \"fr\": 2, \"hi\": 3}")
+	value, found := code5["en"]
+	fmt.Println("value, found := code5[\"en\"]")
+	fmt.Println("value, found = ", value, ",", found)
+	value, found = code5["hh"]
+	fmt.Println("value, found = code5[\"hh\"]")
+	fmt.Println("value, found = ", value, ",", found)
+
+	fmt.Println("\n---- Adding a value to a map")
+	codes6 := map[string]string{"en": "English", "fr": "French", "hi": "Hindi"}
+	fmt.Println("codes6 := map[string]string{\"en\": \"English\", \"fr\": \"French\", \"hi\": \"Hindi\"}")
+	codes6["it"] = "Italian"
+	fmt.Println("codes6[\"it\"] = \"Italian\"")
+	fmt.Println("codes6 =", codes6)
+
+	fmt.Println("\n------ Update a key")
+	fmt.Println("codes6 =", codes6)
+	codes6["en"] = "English language"
+	fmt.Println("codes6[\"en\"] = \"English language\"")
+	fmt.Println("codes6 =", codes6)
+
+	fmt.Println("\n------ Delete a key")
+	fmt.Println("codes6 =", codes6)
+	delete(codes6, "en")
+	fmt.Println("delete(codes6, \"en\")")
+	fmt.Println("codes6 =", codes6)
+
+	fmt.Println("\n------ Interate over a map")
+	fmt.Println("codes6 =", codes6)
+	for key, value := range codes6 {
+		fmt.Println(key, "=>", value)
+	}
+
+	fmt.Println("\n Truncate a map ")
+	fmt.Println("codes6 =", codes6)
+	for key, _ := range codes6 {
+		delete(codes6, key)
+	}
+	fmt.Println("codes6 =", codes6)
+
+	fmt.Println("\n Truncate option #2")
+	codes7 := map[string]string{"en": "English", "fr": "French", "hi": "Hindi"}
+	fmt.Println("codes7 =", codes7)
+	codes7 = make(map[string]string)
+	fmt.Println("codes7 = make(map[string]string)")
+	fmt.Println("codes7 =", codes7)
+
+}
+
+func slices() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("slices {\n\n")
+
+	fmt.Println("a Slice")
+	aSlice := []int{10, 20, 30}
+	fmt.Println("aSlice = ", aSlice)
+
+	fmt.Println("declairing and initializing a slice")
+	aArrays2 := [10]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	fmt.Println("aArrays2 = ", aArrays2)
+	aSlice2 := aArrays2[1:8]
+	fmt.Println(aSlice2)
+	aSubSlice2 := aSlice2[0:3]
+	fmt.Println("aSubSlice2  = aSlice2[0:3] = ", aSubSlice2)
+
+	aSlice3 := make([]int, 5, 8)
+	fmt.Println("aSlice3 := make([]int, 5 , 8) = ", aSlice3)
+	fmt.Println("len(aslice3) =", len(aSlice3))
+	fmt.Println("cap(aslice3) =", cap(aSlice3))
+
+	aArray4 := [10]int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	aSlice4 := aArray4[1:8]
+	fmt.Println("aArray4 = ", aArray4)
+	fmt.Println("aSlice4 := aArray4[1:8]")
+	fmt.Println("aSlice4 = ", aSlice4)
+	fmt.Println("cap(aArray4) = ", cap(aArray4))
+	fmt.Println("cap(aSlice4) = ", cap(aSlice4))
+
+	aArray5 := [5]int{10, 20, 30, 40, 50}
+	aSlice5 := aArray5[:3]
+	fmt.Println("array5 =", aArray5)
+	fmt.Println("aSlice5 := aArray5[:3]")
+	fmt.Println("aSlice5 =", aSlice5)
+	aSlice5[1] = 9000
+	fmt.Println("aSlice5[1] = 9000")
+	fmt.Println("After modification")
+	fmt.Println("array5 =", aArray5)
+	fmt.Println("aSlice5 =", aSlice5)
+
+	fmt.Println("\n\nAppending a slice")
+	aArray6 := [4]int{10, 20, 30, 40}
+	aSlice6 := aArray6[1:3]
+	fmt.Println("aArray6 := [4]int {10,20,30,40}")
+	fmt.Println(" aSlice6 := aArray6[1:3] = ", aSlice6)
+	fmt.Println("len(aSlice6) = ", len(aSlice6))
+	fmt.Println("cap(aSlice6) = ", cap(aSlice6))
+
+	aSlice6 = append(aSlice6, 900, -90, 50)
+	fmt.Println("aSlice6 = append(aSlice6, 900, -90, 50)")
+	fmt.Println(" aSlice6 = ", aSlice6)
+	fmt.Println("len(aSlice6) = ", len(aSlice6))
+	fmt.Println("cap(aSlice6) = ", cap(aSlice6))
+
+	aArray7 := [5]int{10, 20, 30, 40, 50}
+	aSlice7 := aArray7[:2]
+	aArray2a := [5]int{5, 15, 25, 35, 45}
+	ASlice7a := aArray2a[:2]
+
+	aMessage := `
+aArray7 := [5]int{10, 20, 30, 40, 50}
+	aSlice7 := aArray7[:2]
+	aArray2a := [5]int{5, 15, 25, 35, 45}
+	ASlice7a := aArray2a[:2]`
+
+	fmt.Println(aMessage)
+	aNewSlice := append(aSlice7, ASlice7a...)
+	fmt.Println("aNewSlice := append(aSlice7, ASlice7a...)")
+	fmt.Println("aNewSlice", aNewSlice)
+
+	fmt.Println("\n --  Deleting a slice")
+	aArray8 := [5]int{10, 20, 30, 40, 50}
+	fmt.Println("aArray8 := [5]int{10, 20, 30, 40, 50}")
+	i8 := 2
+	fmt.Println("aArray8 = ", aArray8)
+	aSlice8_1 := aArray8[:i8]
+	aSlice8_2 := aArray8[i8+1:]
+	aNewSlice8 := append(aSlice8_1, aSlice8_2...)
+	fmt.Println("aNewSlice8 := append(aSlice8_1, aSlice8_2...)")
+	fmt.Println("aNewSlice8 = ", aNewSlice8)
+
+	fmt.Println("\n---   Copying a slice")
+	src_slice9 := []int{10, 20, 30, 40, 50}
+	dest_slice9 := make([]int, 3)
+	num := copy(dest_slice9, src_slice9)
+	fmt.Println("num := copy(dest_slice9, src_slice9)")
+	fmt.Println("dest_slice9 = ", dest_slice9)
+	fmt.Println("number of elements copied", num)
+
+	fmt.Println("\n ----- Looping through a slice")
+	for index, value := range src_slice9 {
+		fmt.Println(index, "=>", value)
+	}
+
+	fmt.Println("\n ----- Looping through a slice, index not needed")
+	for _, value := range src_slice9 {
+		fmt.Println(value)
+	}
+
+}
+
+func arrays() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("arrays\n")
+
+	var grades [5]int
+	fmt.Println(grades)
+	var fruits [3]string
+
+	fmt.Println(fruits)
+
+	fmt.Println("intialized arrays")
+	var grades2 [3]int = [3]int{10, 20, 30}
+	fmt.Println(grades2)
+	var fruits2 [3]string = [3]string{"apples", "oranges", "grapes"}
+	fmt.Println(fruits2)
+
+	fmt.Println("intialized arrays - shorthand")
+	grades3 := [3]int{10, 20, 30}
+	fmt.Println(grades3)
+
+	fmt.Println("intialized arrays - shorthand ..Ellipses")
+	grades4 := [...]int{10, 20, 30}
+	fmt.Println(grades4)
+
+	fmt.Println("intialized arrays - shorthand ..Ellipses -  stings")
+	fruits3 := [...]string{"apples", "oranges", "grapes"}
+	fmt.Println(fruits3)
+
+	fmt.Println("intialized arrays - shorthand ..Ellipses -  int")
+	marks := [...]int{10, 20, 30}
+	fmt.Println(marks)
+
+	fmt.Println("len function")
+	fmt.Println("len(fruits2) = " + strconv.Itoa(len(fruits2)))
+
+	fmt.Println("Array Indexes")
+	fmt.Println("grades2[1] = " + strconv.Itoa(grades2[1]))
+
+	fmt.Println("Array Indexes #2 strings")
+	fruits5 := [...]string{"apples", "oranges", "grapes", "mango", "papaya"}
+	fmt.Printf("fruits5[2] = %s\n", fruits5[2])
+
+	fmt.Println("Array Indexes #2 int = change value")
+	grades5 := [...]int{90, 80, 70, 80, 97}
+	fmt.Println(grades5)
+	grades5[1] = 100
+	fmt.Println(grades5)
+
+	fmt.Println("loop through array")
+	for i := 0; i < len(grades5); i++ {
+		fmt.Println(grades5[i])
+	}
+
+	fmt.Println("loop through array .. with range")
+	for index, element := range grades5 {
+		fmt.Println(index, "=>", element)
+	}
+
+	fmt.Println("Multi-dementional array")
+	arr1 := [3][2]int{{2, 4}, {4, 16}, {8, 64}}
+	arr2 := [3][2]int{{2, 4}, {4, 16}, {8, 64}}
+	fmt.Println(arr1)
+	fmt.Println("arr1[2][1] = ", arr1[2][1])
+	fmt.Println(arr2)
+	fmt.Println("arr2[2][1] = ", arr2[2][1])
 
 }
 
@@ -42,19 +303,42 @@ func loopControlOperator() {
 	fmt.Println("\n----------------------------------------")
 	fmt.Println("Loop Control functions\n")
 
+	fmt.Println("-------------")
+	fmt.Println("For loop ")
 	for i := 1; i <= 3; i++ {
 		fmt.Println("Hello World")
 	}
 
+	fmt.Println("-------------")
+	fmt.Println("For loop ")
 	for i := 1; i <= 5; i++ {
 		fmt.Println(i * i)
 	}
 
 	fmt.Println("-------------")
+	fmt.Println("For loop no initialization or post increment")
 	var i int = 1
 	for i <= 5 {
 		fmt.Println(i * i)
-		i++
+		i += 1
+	}
+
+	fmt.Println("-------------")
+	fmt.Println("If with break .. ends with 3")
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+		if i == 3 {
+			break
+		}
+	}
+
+	fmt.Println("-------------")
+	fmt.Println("If with continue .. skips 3")
+	for i := 1; i <= 5; i++ {
+		if i == 3 {
+			continue
+		}
+		fmt.Println(i)
 	}
 
 }
