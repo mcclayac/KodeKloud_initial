d28dcf96b36a14524a302f477dd4ff4dc42b1a41
Anthony McClay <anthony.mcclay@gmail.com>
2022-02-22 04:01:24 -0600

    Interfaces

--- main.go
+++ main.go
@@ -55,6 +55,53 @@ func structsMethodsInterfaces() {
 	methods()
 	methodSets()
 
+	interfaces()
+
+}
+
+func interfaces() {
+	fmt.Println("\n----------------------------------------")
+	fmt.Println("interfaces \n")
+
+	r := rect{length: 3, breath: 4}
+	s := square{side: 5}
+	printData(r)
+	printData(s)
+}
+
+type shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type square struct {
+	side float64
+}
+
+func (s square) area() float64 {
+	return s.side * s.side
+}
+
+func (s square) perimeter() float64 {
+	return 4 * s.side
+}
+
+type rect struct {
+	length, breath float64
+}
+
+func (r rect) area() float64 {
+	return r.length * r.breath
+}
+
+func (r rect) perimeter() float64 {
+	return 2*r.length + 2*r.breath
+}
+
+func printData(s shape) {
+	fmt.Println(s)
+	fmt.Println(s.area())
+	fmt.Println(s.perimeter())
 }
 
 func methodSets() {
